2355b1a4e67e323fb6500568171d2f911292a1e3
Leonard-B212 <Bertsch.Leonard@web.de>
2026-07-19 22:31:28 +0200

    AdminBootstrap created

---
+++ auth-service/src/main/java/de/lernspiel/auth/config/AdminBootstrap.java
@@ -0,0 +1,43 @@
+package de.lernspiel.auth.config;
+
+import de.lernspiel.auth.entity.User;
+import de.lernspiel.auth.entity.UserType;
+import de.lernspiel.auth.repository.UserRepository;
+import de.lernspiel.auth.service.UserService;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class AdminBootstrap {
+
+    private static final Logger logger = LoggerFactory.getLogger(AdminBootstrap.class);
+
+    @Value("${app.bootstrap-admin.password}")
+    private String adminPassword;
+
+    @Bean
+    public CommandLineRunner createAdmin(UserRepository userRepository, UserService userService) {
+        return args -> {
+
+            if (userRepository.existsByType(UserType.ADMIN)) {
+                logger.info("Bootstrap admin already exists.");
+                return;
+            }
+
+            User admin = new User();
+            admin.setPassword(adminPassword);
+            admin.setType(UserType.ADMIN);
+            admin.setClassID(null);
+
+            User savedAdmin = userService.addUser(admin);
+
+            logger.info("Bootstrap admin created successfully (ID: {}).", savedAdmin.getUserID());
+        };
+    }
+}
--- auth-service/src/main/java/de/lernspiel/auth/repository/UserRepository.java
+++ auth-service/src/main/java/de/lernspiel/auth/repository/UserRepository.java
@@ -11,4 +11,5 @@ import java.util.Optional;
  */
 
 public interface UserRepository extends JpaRepository<User, Integer> {
+    boolean existsByType(UserType type);
 }
