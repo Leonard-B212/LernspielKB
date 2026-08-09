e8ac7260fee9059b56c30b60a6fc02fd4f377125
Philipp Krings <pjkrings05@gmail.com>
2026-08-09 18:59:33 +0200

    Implemented full Variable Declaration Logic

--- game-service/src/main/java/de/lernspiel/game/service/InterpreterService.java
+++ game-service/src/main/java/de/lernspiel/game/service/InterpreterService.java
@@ -9,6 +9,8 @@ import org.springframework.stereotype.Service;
 import de.lernspiel.game.dto.CodeBlock;
 import de.lernspiel.game.dto.CodeType;
 import de.lernspiel.game.dto.ProgramRequest;
+import de.lernspiel.game.dto.ValueBlock;
+import de.lernspiel.game.dto.VarNameBlock;
 import de.lernspiel.game.dto.Variable;
 
 @Service
@@ -61,38 +63,107 @@ public class InterpreterService {
 
     public void variableDeclaration(CodeBlock[] lineOfCode, Map<String, Variable<?>> variables){
         CodeBlock variableType = lineOfCode[0];
-        CodeBlock variableName;
-        CodeBlock variableValue;
+        VarNameBlock variableName;
+        ValueBlock variableValue;
 
         if(lineOfCode.length < 2){
             //TODO: Errorhandling
+            throw new IllegalArgumentException("");
         } else if(!lineOfCode[1].getType().equals(CodeType.VAR_NAME)){
             //TODO: Errorhandling
+            throw new IllegalArgumentException("");
         } else {
-            variableName = lineOfCode[1];
+            variableName = (VarNameBlock) lineOfCode[1];
         }
 
         if(lineOfCode.length < 3){
             //TODO: Errorhandling
+            throw new IllegalArgumentException("");
         } else if(lineOfCode[2].getType().equals(CodeType.BREAK)){
-            //TODO: Variable Deklariert ohne Value Zuweisung
-        } else if(!lineOfCode[2].getType().equals(CodeType.EQUALS)){
-            //TODO: Errorhandling
-        } else {
-            if(lineOfCode.length < 4){
+            //Variable Deklariert ohne Value Zuweisung
+
+            if(variables.get(variableName.getName()).equals(null)){
                 //TODO: Errorhandling
-            } else if(!lineOfCode[3].getType().equals(CodeType.VALUE)){
-                //TODO: Errorhandling
-            } else {
-                variableValue = lineOfCode[3];
-                if(lineOfCode.length < 5){
-                    //TODO: Errorhandling
-                } else if(!lineOfCode[4].getType().equals(CodeType.BREAK)){
-                    //TODO: Errorhandling
-                } else {
-                    //TODO: Variable deklariert und Value assigned
-                }
+                throw new IllegalArgumentException("Variable bereits deklariert");
             }
+
+            switch (variableType.getType()) {
+                case STRING:
+                    Variable<String> strVariable = new Variable<>(null, String.class);
+                    variables.put(variableName.getName(), strVariable);
+                    return;
+                case INT:
+                    Variable<Integer> intVariable = new Variable<>(null, Integer.class);
+                    variables.put(variableName.getName(), intVariable);
+                    return;
+                case BOOLEAN:
+                    Variable<Boolean> boolVariable = new Variable<>(null, Boolean.class);
+                    variables.put(variableName.getName(), boolVariable);
+                    return;
+                default:
+                    //TODO: Errorhandling
+                    throw new IllegalArgumentException("");
+            }
+        }
+            
+        if(!lineOfCode[2].getType().equals(CodeType.EQUALS)){
+            //TODO: Errorhandling
+            throw new IllegalArgumentException("");
+        } else if(lineOfCode.length < 4){
+            //TODO: Errorhandling
+            throw new IllegalArgumentException("");
+        } else if(!lineOfCode[3].getType().equals(CodeType.VALUE)){
+            //TODO: Errorhandling
+            throw new IllegalArgumentException("");
+        }
+        
+        variableValue = (ValueBlock) lineOfCode[3];
+
+        if(lineOfCode.length < 5){
+            //TODO: Errorhandling
+            throw new IllegalArgumentException("");
+        } else if(!lineOfCode[4].getType().equals(CodeType.BREAK)){
+            //TODO: Errorhandling
+            throw new IllegalArgumentException("");
+        }
+
+        //Variable deklariert und Value assigned
+        if(variables.get(variableName.getName()).equals(null)){
+            //TODO: Errorhandling
+            throw new IllegalArgumentException("Variable bereits deklariert");
+        }
+
+        switch (variableType.getType()) {
+            case STRING:
+                if(variableValue.getValue().getType().equals(String.class)){
+                    String value = (String) variableValue.getValue().getValue();
+                    Variable<String> strVariable = new Variable<>(value, String.class);
+                    variables.put(variableName.getName(), strVariable);
+                    return;
+                }
+                //TODO: Errorhandling
+                throw new IllegalArgumentException("");
+            case INT:
+                if(variableValue.getValue().getType().equals(Integer.class)){
+                    Integer value = (Integer) variableValue.getValue().getValue();
+                    Variable<Integer> intVariable = new Variable<>(value, Integer.class);
+                    variables.put(variableName.getName(), intVariable);
+                    return;
+                }
+                //TODO: Errorhandling
+                throw new IllegalArgumentException("");
+            case BOOLEAN:
+                if(variableValue.getValue().getType().equals(Boolean.class)){
+                    Boolean value = (Boolean) variableValue.getValue().getValue();
+                    Variable<Boolean> boolVariable = new Variable<>(value, Boolean.class);
+                    variables.put(variableName.getName(), boolVariable);
+                    return;
+                }
+                //TODO: Errorhandling
+                throw new IllegalArgumentException("");
+            default:
+                //TODO: Errorhandling
+                throw new IllegalArgumentException("");
         }
     }
 
