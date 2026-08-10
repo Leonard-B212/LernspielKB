2c13cedf631b6dc2d2e033e4994258202784e968
Philipp Krings <pjkrings05@gmail.com>
2026-08-10 16:30:45 +0200

    JSON Type Annotations

--- game-service/src/main/java/de/lernspiel/game/dto/CodeBlock.java
+++ game-service/src/main/java/de/lernspiel/game/dto/CodeBlock.java
@@ -1,5 +1,20 @@
 package de.lernspiel.game.dto;
 
+import com.fasterxml.jackson.annotation.JsonSubTypes;
+import com.fasterxml.jackson.annotation.JsonTypeInfo;
+
+@JsonTypeInfo(
+    use = JsonTypeInfo.Id.NAME,
+    include = JsonTypeInfo.As.EXISTING_PROPERTY,
+    property = "type",
+    visible = true
+)
+@JsonSubTypes({
+    @JsonSubTypes.Type(value = VarNameBlock.class, name = "VAR_NAME"),
+    @JsonSubTypes.Type(value = ValueBlock.class, name = "VALUE"),
+    @JsonSubTypes.Type(value = IfStatementBlock.class, name = "IF_STATEMENT"),
+    @JsonSubTypes.Type(value = ElseStatementBlock.class, name = "ELSE_STATEMENT")
+})
 public class CodeBlock {
     private CodeType type;
 
