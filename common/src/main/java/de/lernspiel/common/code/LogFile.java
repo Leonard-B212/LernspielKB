6a67e8b49d4d1053bd487d761e9a038c149aa11a
Leonard-B212 <Bertsch.Leonard@web.de>
2026-09-18 14:21:46 +0200

    Fixed LogFile constructor

--- common/src/main/java/de/lernspiel/common/code/LogFile.java
+++ common/src/main/java/de/lernspiel/common/code/LogFile.java
@@ -2,10 +2,17 @@ package de.lernspiel.common.code;
 
 import java.util.Map;
 
+/**
+ * Repräsentiert einen einzelnen Eintrag innerhalb eines ExecutionLogs.
+ */
 public class LogFile {
+
     private Map<String, Object> contents;
     private LogType logType;
     private boolean previewed;
+
+    public LogFile() {
+    }
 
     public LogFile(Map<String, Object> contents, LogType logType) {
         this(contents, logType, false);
@@ -17,10 +24,27 @@ public class LogFile {
         this.previewed = previewed;
     }
 
-    public Map<String, Object> getContents() { return contents; }
-    public void setContents(Map<String, Object> contents) { this.contents = contents; }
-    public LogType getLogType() { return logType; }
-    public void setLogType(LogType logType) { this.logType = logType; }
-    public boolean isPreviewed() { return previewed; }
-    public void setPreviewed(boolean previewed) { this.previewed = previewed; }
+    public Map<String, Object> getContents() {
+        return contents;
+    }
+
+    public void setContents(Map<String, Object> contents) {
+        this.contents = contents;
+    }
+
+    public LogType getLogType() {
+        return logType;
+    }
+
+    public void setLogType(LogType logType) {
+        this.logType = logType;
+    }
+
+    public boolean isPreviewed() {
+        return previewed;
+    }
+
+    public void setPreviewed(boolean previewed) {
+        this.previewed = previewed;
+    }
 }
